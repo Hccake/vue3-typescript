fa5959dcb23b5448c598e99df0d9ae219f4360c5
Hccake <chengbohua@foxmail.com>
2023-04-20 14:37:41 +0800

    :tada: init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MyButton from "./components/MyButton.vue";
+</script>
+
+<template>
+    <my-button text="测试按钮" />
+</template>
+
+
---
+++ src/components/MyButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type {ButtonProps} from "ant-design-vue";
+interface MyButtonProps extends ButtonProps {
+    text: string
+}
+
+const props = defineProps<MyButtonProps>();
+</script>
+
+<template>
+    <a-button> {{ props.text }} </a-button>
+</template>
